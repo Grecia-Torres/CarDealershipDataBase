f9765240a5b3166b14f058e105a1c1120c973870
Grecia-Torres <grecia.atorres@gmail.com>
2024-11-21 11:19:34 -0800

    finished CarDealershipDataBase

--- CarDealership.sql
+++ CarDealership.sql
@@ -8,9 +8,84 @@ CREATE DATABASE IF NOT EXISTS CarDealership;
 
 USE CarDealership;
 
-# ---------------------------------------------------------------------- #
-# Tables                                                                 #
-# ---------------------------------------------------------------------- #
-# ---------------------------------------------------------------------- #
-# Add table "Dealerships"                                                 #
-# ---------------------------------------------------------------------- #
+CREATE TABLE Dealerships (
+    DealershipID INT NOT NULL AUTO_INCREMENT,
+    `Name` VARCHAR(50),
+    Address VARCHAR(50),
+    Phone VARCHAR(12),
+    PRIMARY KEY (DealershipID)
+);
+
+CREATE TABLE Vehicles (
+    VIN VARCHAR(17) NOT NULL,
+    Color VARCHAR(50),
+    Make VARCHAR(50),
+    Model VARCHAR(50),
+    Sold BOOLEAN,
+    PRIMARY KEY (VIN)
+);
+CREATE TABLE Inventory (
+    DealershipID INT NOT NULL,
+    VIN VARCHAR(17) NOT NULL,
+    PRIMARY KEY (DealershipID , VIN)
+);
+
+CREATE TABLE SalesContracts (
+    ID INT AUTO_INCREMENT,
+    SaleDate DATE,
+    Price DECIMAL(9 , 2 ),
+    VIN VARCHAR(20),
+    PRIMARY KEY (ID),
+    FOREIGN KEY (VIN)
+        REFERENCES Vehicles (VIN)
+);
+
+CREATE TABLE LeaseContracts (
+    ID INT AUTO_INCREMENT,
+    VIN VARCHAR(17) NOT NULL,
+    LeaseDate DATE,
+    MonthlyPayment DECIMAL(9 , 2 ),
+    PRIMARY KEY (ID),
+    FOREIGN KEY (VIN)
+        REFERENCES Vehicles (VIN)
+);
+
+INSERT INTO Dealerships (`Name`, ADDRESS, PHONE) VALUES
+('Torres Auto', '223 Long St, San Francisco, CA', '415-551-6678'),
+('Mar Auto', '451 Rhine St, San Francisco, CA', '415-442-7778'),
+('Towers Auto', '333 Vista St, San Francisco, CA', '415-332-9978'),
+('Alex Auto', '545 James St, San Francisco, CA', '415-222-5578');
+
+INSERT INTO Vehicles (VIN, Color, Make, Model, Sold) VALUES
+('1HGCM82633A123451','Red','Honda','Fit',FALSE),
+('1HGCM82633A123452','Green','Toyota','Camry',TRUE),
+('1HGCM82633A123453','Silver','Ford','Bronco',TRUE),
+('1HGCM82633A123454','Orange','Honda','Accord',FALSE),
+('1HGCM82633A123455','Pink','Kia','Soul',TRUE),
+('1HGCM82633A123456','Red','BMW','A9',TRUE),
+('1HGCM82633A123457','Blue','Tesla','Model 3',FALSE),
+('1HGCM82633A123458','Black','Honda','Civic',TRUE);
+
+INSERT INTO Inventory (DealershipID, VIn) VALUES
+(1,'1HGCM82633A123451'),
+(2,'1HGCM82633A123452'),
+(3,'1HGCM82633A123453'),
+(1,'1HGCM82633A123454'),
+(2,'1HGCM82633A123455'),
+(3,'1HGCM82633A123456'),
+(5,'1HGCM82633A123457'),
+(4,'1HGCM82633A123458');
+
+INSERT INTO SalesContracts (SaleDate,Price, VIN) VALUES
+('2024-01-03', '43000.00', '1HGCM82633A123455'),
+('2024-05-13', '3000.00','1HGCM82633A123456'),
+('2024-06-24', '24000.00','1HGCM82633A123457'),
+('2024-07-15', '55000.00','1HGCM82633A123458');
+
+INSERT INTO LeaseContracts (VIN, LeaseDate, MonthlyPayment) VALUES
+('1HGCM82633A123451','2024-08-24','240.00'),
+('1HGCM82633A123452','2024-09-12','450.00'),
+('1HGCM82633A123453','2024-10-13','550.00'),
+('1HGCM82633A123454','2024-11-15','327.00');
+
+
--- Test.sql
+++ Test.sql
@@ -1,11 +1,63 @@
--- 1
+-- 1 Get all dealerships
+SELECT 
+    *
+FROM
+    Dealerships;
 
 -- 2
+SELECT 
+    Vehicles.*, `Name`
+FROM
+    Inventory
+        INNER JOIN
+    Vehicles ON Inventory.VIN = Vehicles.VIN
+        INNER JOIN
+    Dealerships ON Inventory.DealershipID = Dealerships.DealershipID
+WHERE
+    Inventory.DealershipID = '2';
 
 -- 3
+SELECT 
+    *
+FROM
+    Vehicles
+WHERE
+    VIN = '1HGCM82633A123452';
 
 -- 4
+SELECT 
+    `Name`, Address
+FROM
+    Dealerships
+        INNER JOIN
+    Inventory ON Dealerships.DealershipID = Inventory.DealershipID
+WHERE
+    Inventory.VIN = '1HGCM82633A123454';
 
 -- 5
+SELECT 
+    `Name`, Color, Make, Model
+FROM
+    Dealerships
+        INNER JOIN
+    Inventory ON Dealerships.DealershipID = Inventory.DealershipID
+        INNER JOIN
+    Vehicles ON Inventory.VIN = Vehicles.VIN
+WHERE
+    Vehicles.Color = 'Red'
+        AND Vehicles.Make = 'Honda'
+        AND Vehicles.Model = 'Fit';
 
 -- 6
+SELECT 
+    `Name`, SalesContracts.*
+FROM
+    SalesContracts
+        INNER JOIN
+    Inventory ON SalesContracts.VIN = Inventory.VIN
+        INNER JOIN
+    Dealerships ON Inventory.DealershipID = Dealerships.DealershipID
+WHERE
+    Dealerships.DealershipID = '1'
+        AND SalesContracts.SaleDate BETWEEN '2024-01-01' AND '2024-11-28';
+
